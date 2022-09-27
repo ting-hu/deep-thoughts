0eb07f68c25f3f63403a33821748d03a33be352e
ting-hu <huxu@uwplatt.edu>
2022-09-27 11:40:19 -0500

    Using faker

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,13 @@
 const mongoose = require("mongoose");
 
 mongoose
-  .connect(process.env.MONGODB_URI || "mongodb://localhost/deep-thoughts", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(
+    process.env.MONGODB_URI || "mongodb://localhost:27017/deep-thoughts",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
   .catch((err) => console.log(err.reason));
 
 module.exports = mongoose.connection;
