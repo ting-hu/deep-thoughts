718e5fa162251aa62740344a3db517e4405fc939
ting-hu <huxu@uwplatt.edu>
2022-10-04 11:25:52 -0500

    Color

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React from "react";
 
 const Footer = () => {
   return (
     <footer className="w-100 mt-auto bg-secondary p-4">
       <div className="container">
-        &copy;{new Date().getFullYear()} by Lernantino
+        &copy;{new Date().getFullYear()} by Ting Hu
       </div>
     </footer>
   );
